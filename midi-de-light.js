efb1f639f09f392820d3be2f461b4c2953ea0eeb
asari-koto <asari-koto@kayac.com>
2016-07-27 08:05:01 +0900

    ripple表現追加

--- midi-de-light.js
+++ midi-de-light.js
@@ -4,12 +4,15 @@ var MidiDeLight = new function() {
 	var KEY_RANGE = {left: 36, right: 96};
 	//var KEY_RANGE = {left: 36, right: 84};
 	var PARTICLE_SIZE = window.innerWidth/(KEY_RANGE.right - KEY_RANGE.left + 1);
+	var MAX_EXTEND_SPEED = 7;
 
 	var p = [];
 	var isFirst = [];
 	for (var i=0;i<128;i++) {
 		isFirst[i] = true;
 	}
+
+	var c = [];
 
 	this.init = function() {
 		canvas = document.getElementById('world');
@@ -77,6 +80,7 @@ var MidiDeLight = new function() {
 		for (var i=0; i<p.length; i++) {
 			if (p[i].alpha <= 0) {
 				p.splice(i, 1);
+				i--;
 				continue;
 			}
 			ctx.fillStyle = getRGBString(p[i].color.r, p[i].color.g, p[i].color.b, p[i].alpha);
@@ -85,6 +89,36 @@ var MidiDeLight = new function() {
 			ctx.arc(p[i].x, p[i].y, p[i].r, 0, Math.PI*2, true);
 			ctx.fill();
 		}
+	}
+
+	function extendCircle() {
+	    for (var i=0; i<c.length; i++) {
+			c[i].r += MAX_EXTEND_SPEED;
+			c[i].alpha -= 0.02;
+		}
+    }
+
+    function createCircle(x, y, r, color) {
+	    c.push( new Circle(x, y, r, color) );
+	}
+
+	function drawCircle() {
+		ctx.shadowBlur = 20;
+
+		for (var i=0; i<c.length; i++) {
+			if (c[i].alpha <= 0) {
+				c.splice(i, 1);
+				i--;
+				continue;
+			}
+
+			ctx.lineWidth = c[i].alpha * 10;
+			ctx.strokeStyle = getRGBString(c[i].color.r, c[i].color.g, c[i].color.b, 1);
+			ctx.beginPath();
+			ctx.arc(c[i].x, c[i].y, c[i].r, 0, Math.PI*2, false);
+			ctx.stroke();
+		}
+		ctx.shadowBlur = 0;
 	}
 
 	function loop() {
@@ -115,6 +149,7 @@ var MidiDeLight = new function() {
 				ctx.fill();
 				if (isFirst[i]) {
 					createParticle(x, y, size, midiKey[i], {r: r, g: g, b: b});
+					createCircle(x, y, 0, {r: r, g: g, b: b});
 					isFirst[i] = false;
 				}
 
@@ -125,6 +160,9 @@ var MidiDeLight = new function() {
 
 		moveParticle();
 		drawParticle();
+
+		extendCircle();
+		drawCircle();
 	}
 
 	function Particle(r, x, y, dx, dy, color) {
@@ -133,6 +171,14 @@ var MidiDeLight = new function() {
 		this.y = y;
 		this.dx = dx;
 		this.dy = dy;
+		this.color = color;
+		this.alpha = 1;
+	}
+
+	function Circle(x, y, r, color) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
 		this.color = color;
 		this.alpha = 1;
 	}
